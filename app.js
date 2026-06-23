d81133e22b1a6a4793423f1f218f3d0a9e44843f
unknown <mohamedishaaqmeeran@gmail.com>
2026-06-23 17:22:06 +0530

    ad

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ app.use(
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
