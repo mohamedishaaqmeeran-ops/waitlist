bb8f7ff4e8453a21cbe05933465910537cd7cc4c
unknown <mohamedishaaqmeeran@gmail.com>
2026-06-23 16:50:51 +0530

    aa'

--- models/waitlistModel.js
+++ models/waitlistModel.js
@@ -28,4 +28,4 @@ const waitlistSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("user", waitlistSchema);
+module.exports = mongoose.model("User", waitlistSchema);
